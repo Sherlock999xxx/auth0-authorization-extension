2be4292160856b7f8edc421c768437ae20dcff99
Tim Saunders <tim.saunders@okta.com>
2026-01-09 14:18:59 +0000

    Fix seriesQueue

--- server/lib/storage/contexts/seriesQueue.js
+++ server/lib/storage/contexts/seriesQueue.js
@@ -11,10 +11,26 @@ module.exports = function seriesQueue() {
 
     running = true;
     const task = queue.shift();
-    task.action(function(err, res) {
+
+    // Handle both callback-style and Promise-returning functions
+    const result = task.action(function(err, res) {
+      // Callback-style: action will call this callback
       task.callback(err, res);
       next();
     });
+
+    // If action returns a Promise (doesn't use the callback), handle it
+    if (result && typeof result.then === 'function') {
+      result
+        .then(function(res) {
+          task.callback(null, res);
+          next();
+        })
+        .catch(function(err) {
+          task.callback(err);
+          next();
+        });
+    }
   };
 
   return function(action, callback) {
