0fb2a80e9a53716c7167aaa856e5b22a0679bb20
Tim Saunders <tim.saunders@okta.com>
2026-01-13 12:40:15 +0000

    Tests for session manager

--- tests/unit/mocks/tokens.js
+++ tests/unit/mocks/tokens.js
@@ -1,12 +1,53 @@
 import nock from 'nock';
 import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 import certs from './certs.json';
 import config from '../../../server/lib/config';
 
-const cert = certs.test;
+const defaultCert = certs.test;
 
-module.exports.wellKnownEndpoint = (domain, kid) =>
-   nock(`https://${domain}`)
+// Extract n and e from a PEM certificate
+function extractRsaPublicKeyComponents(certPem) {
+  const certObj = crypto.createPublicKey({
+    key: certPem,
+    format: 'pem'
+  });
+  const jwk = certObj.export({ format: 'jwk' });
+  return {
+    n: jwk.n,
+    e: jwk.e
+  };
+}
+
+module.exports.wellKnownEndpoint = (domain, kidOrCert, kid) => {
+   // Support both old signature (domain, kid) and new signature (domain, cert, kid)
+   let cert;
+   let keyId;
+   let certPem;
+
+   if (kid === undefined) {
+     // Old signature: (domain, kid)
+     cert = defaultCert;
+     keyId = kidOrCert;
+     certPem = cert.cert;
+   } else {
+     // New signature: (domain, cert, kid)
+     // cert can be a string (cert name like 'bar') or an object (cert data)
+     if (typeof kidOrCert === 'string') {
+       cert = certs[kidOrCert];
+       certPem = cert.cert;
+     } else {
+       // kidOrCert is the actual cert PEM string
+       certPem = kidOrCert;
+       cert = null;
+     }
+     keyId = kid;
+   }
+
+   // Extract RSA components from certificate
+   const rsaComponents = extractRsaPublicKeyComponents(certPem);
+
+   return nock(`https://${domain}`)
     .get('/.well-known/jwks.json')
     .reply(200, {
       keys: [
@@ -14,22 +55,21 @@ module.exports.wellKnownEndpoint = (domain, kid) =>
           alg: 'RS256',
           use: 'sig',
           kty: 'RSA',
-          x5c: [ cert.cert.match(/-----BEGIN CERTIFICATE-----([\s\S]*)-----END CERTIFICATE-----/i)[1].replace('\n', '') ],
-          kid,
-          n: cert.modulus,
-          e: cert.exponent,
-          x5t: cert.fingerprint
+          x5c: [ certPem.match(/-----BEGIN CERTIFICATE-----([\s\S]*)-----END CERTIFICATE-----/i)[1].replace(/[\r\n]/g, '') ],
+          kid: keyId,
+          n: rsaComponents.n,
+          e: rsaComponents.e
         }
       ]
-    })
-;
+    });
+};
 
 module.exports.sign = (certArg, kid, payload) =>
    jwt.sign(payload, certArg, { header: { kid }, algorithm: 'RS256' })
 ;
 
 module.exports.getToken = (scope) =>
-   module.exports.sign(cert.privateKey, 'key2', {
+   module.exports.sign(defaultCert.privateKey, 'key2', {
      iss: `https://${config('AUTH0_DOMAIN')}/`,
      aud: 'urn:auth0-authz-api',
      sub: '123456@clients',
@@ -38,7 +78,7 @@ module.exports.getToken = (scope) =>
 ;
 
 module.exports.getUserToken = (scope) =>
-   module.exports.sign(cert.privateKey, 'key2', {
+   module.exports.sign(defaultCert.privateKey, 'key2', {
      iss: `https://${config('AUTH0_DOMAIN')}/`,
      aud: 'urn:auth0-authz-api',
      sub: 'auth0|aaaaaaaaa',
@@ -58,7 +98,7 @@ module.exports.getAdminTokenWithoutAccessToken = (scope) =>
 ;
 
 module.exports.getApiToken = (gty, sub, scope) =>
-  module.exports.sign(cert.privateKey, 'key2', {
+  module.exports.sign(defaultCert.privateKey, 'key2', {
     iss: `https://${config('AUTH0_DOMAIN')}/`,
     aud: 'urn:auth0-authz-api',
     sub: `auth0@${sub}`,
